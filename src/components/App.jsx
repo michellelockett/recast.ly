0e706a377b523014dd4405415bc6e106dc5cc3ad
Michelle Lockett <shelldog42@gmail.com>
2017-12-09 14:55:29 -0800

    Renders fake video data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+const TESTDATA = window.exampleVideoData;
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,7 +12,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos={TESTDATA}/>
       </div>
     </div>
   </div>
@@ -19,3 +21,4 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+ReactDOM.render(<App />, document.getElementById('app'));
